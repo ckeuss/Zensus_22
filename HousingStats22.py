8d311b6bf62b67de0ea881d0f950a322d2635e2c
Claudia <147528104+ckeuss@users.noreply.github.com>
2025-03-19 23:20:18 +0100

    add library

--- HousingStats22.py
+++ HousingStats22.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-
+import openpyxl
 
 # Streamlit set up
 st.set_page_config(page_title="Housing statistics 2022", layout = "wide")
